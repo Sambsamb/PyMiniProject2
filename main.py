858cdf13c19c6c964fe70642ff1ac36719ec78f5
Samb <samb@townsware.com>
2022-09-20 11:18:11 -0400

    testing

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # 9/20/2022 - INF601 - Advanced Python - Pandas class - Week 5
 # pip install -r requirements.txt
-# requirements.txt ==> pandas>=1.5.0, openpyxl>=3.0.10, Faker>=14.2.0
+# requirements.txt ==> pandas>=1.5.0, openpyxl>=3.0.10, Faker>=14.2.0, requests>=2.28.1
 
 import pandas as pd
+import requests
+import json
 
 # Manual creation of DF:
 df = pd.DataFrame(
@@ -19,4 +21,12 @@ df = pd.DataFrame(
 
 # Out to Excel:
 df.to_excel("Sample1.xlsx", sheet_name="passengers", index=False)
+
+# Get JSON data from FCC website
+data = requests.get("https://opendata.fcc.gov/resource/bzun-59r8.json")
+data.status_code #  200
+myjson = data.json()
+print(myjson)
+
+# Or:
 
