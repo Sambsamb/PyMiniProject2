cd61121585b111760c1b8d1c4be8f800d538acb0
Samb <samb@townsware.com>
2022-09-22 10:06:15 -0400

    testing

--- main.py
+++ main.py
@@ -11,3 +11,27 @@ Mini Project 2
 import pandas as pd
 import requests
 import json
+
+
+# (20) Using a data source of your choice, retrieve some data
+data = requests.get("https://opendata.fcc.gov/resource/bzun-59r8.json")
+data.status_code #  200
+myjson = data.json()
+
+
+# (10) Store this information in Pandas dataframe
+
+
+
+
+# (10) Using matplotlib, graph this data in a way that will visually represent the data
+
+
+
+
+# (10) Save these graphs in a folder called charts as PNG files
+
+
+
+
+
